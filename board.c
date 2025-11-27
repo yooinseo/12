0488cb7a8099807776d840487219eefe21af0cd1
yooinseo <qetu37@sookmyung.ac.kr>
2025-11-27 23:23:01 +0900

    Update board.c

--- board.c
+++ board.c
@@ -1,15 +1,4 @@
-void board_initBoard(void);
-int board_getBoardStatus(int pos);
-int board_getSharkPosition(void);
-int board_stepShark(void);
-int board_getBoardCoin(int pos);
-void board_printBoardStatus(void);
-
-#define N_BOARD                   20
-#define BOARDSTATUS_OK            1
-#define BOARDSTATUS_NOK           0
-#define N_COINPOS                 12
-#define MAX_COIN                  4
+#include "board.h"
 
 static int board_status[N_BOARD];
 static int board_coin[N_BOARD];
@@ -17,27 +6,25 @@ static int shark_position;
 
 void board_printBoardStatus(void)
 {
-     int i;
-     
-     printf("----------------- BOARD STATUS -----------------\n");
-     for (i=0;i<N_BOARD;i++)
-     {
-         printf("|");
-         if (board_status[i] == BOARDSTATUS_NOK)
+    int i;
+    printf("--------------- BOARD STATUS ---------------\n");
+    for (i = 0; i < N_BOARD; i++){
+        printf("|");
+        if (board_status[i] == BOARDSTATUS_NOK)
             printf("X");
-         else
-             printf("O");                 
-     }
-     printf("|\n");
-     printf("------------------------------------------------\n");
-     
-     printf("----------------- BOARD COIN -----------------\n");
-     for (i=0;i<N_BOARD;i++)
-     {
-         printf("|%i", board_coin[i]);
-     }
-     printf("|\n");
-     printf("------------------------------------------------\n");
+        else
+            printf("O");
+    }
+    printf("|\n");
+    printf("--------------------------------------------\n");
+
+    printf("--------------- BOARD COIN -----------------\n");
+    for (i = 0; i < N_BOARD; i++)
+        printf("|%d", board_coin[i]);
+    printf("|\n");
+    printf("--------------------------------------------\n");
+
+    printf("Shark Position : %d\n", shark_position);
 }
 
 int board_getBoardStatus(int pos)
@@ -52,26 +39,43 @@ int board_getBoardCoin(int pos)
     return coin;
 }
 
+int board_getSharkPosition(void)
+{
+    return shark_position;
+}
+
+int board_stepShark(void)
+{
+    int step = rand() % MAX_SHARKSTEP + 1;
+    int i;
+
+    for (i = 0; i < step; i++){
+        if (shark_position + 1 >= N_BOARD) break;
+        shark_position++;
+        board_status[shark_position] = BOARDSTATUS_NOK;
+    }
+    return shark_position;
+}
+
 void board_initBoard(void)
 {
     int i;
-    
-    for (i=0;i<N_BOARD;i++)
-    {
+
+    for (i = 0; i < N_BOARD; i++){
         board_status[i] = BOARDSTATUS_OK;
         board_coin[i] = 0;
     }
-    
-    for (i=0;i<N_COINPOS;i++)
-    {
-        int flag_allocated=0;
-        do{
-            int coinpos = rand()%N_BOARD;
-            if (board_coin[coinpos] == 0)
-            {
-                                    board_coin[coinpos] = rand()%MAX_COIN + 1;
-                                    flag_allocated = 1;
+
+    shark_position = SHARK_INITPOS;
+
+    for (i = 0; i < N_COINPOS; i++){
+        int flag = 0;
+        while (!flag){
+            int pos = rand() % N_BOARD;
+            if (board_coin[pos] == 0){
+                board_coin[pos] = rand() % MAX_COIN + 1;
+                flag = 1;
             }
-        } while(flag_allocated == 0);
+        }
     }
 }
